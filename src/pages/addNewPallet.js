c22048cefdaab781d848bded367eb5198e1aace4
SVEN <92024717+svendev1222@users.noreply.github.com>
2022-08-17 11:10:42 -0500

    update

--- src/pages/addNewPallet.js
+++ src/pages/addNewPallet.js
@@ -15,24 +15,6 @@ export default function AddNewPallet() {
 
         <Grid container spacing={3}>
           <PalletInfoForm id={id.id} />
-            
-          {/* <Grid item xs={12} md={6} lg={4}>
-            <AppCurrentVisits
-              title="Current Visits"
-              chartData={[
-                { label: 'America', value: 4344 },
-                { label: 'Asia', value: 5435 },
-                { label: 'Europe', value: 1443 },
-                { label: 'Africa', value: 4443 },
-              ]}
-              chartColors={[
-                theme.palette.primary.main,
-                theme.palette.chart.blue[0],
-                theme.palette.chart.violet[0],
-                theme.palette.chart.yellow[0],
-              ]}
-            />
-          </Grid> */}
         </Grid>
       </Container>
     </Page>
